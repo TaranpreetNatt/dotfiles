4cfe4bb0a2c09a584e0e6a5670dcbdb517ca5728
Taranpreet Natt <taranpreetnatt@Taranpreets-MBP.lan>
2024-02-06 22:14:35 -0800

    Neovim Lazyvim distribution configured

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,3 +1,27 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+--
+vim.keymap.set("n", "<C-d>", "<C-d>zz", {
+  desc = "Move half a page down and center the screen",
+  noremap = true,
+  silent = true,
+})
+
+vim.keymap.set("n", "<C-u>", "<C-u>zz", {
+  desc = "Move half a page up and center the screen",
+  noremap = true,
+  silent = true,
+})
+
+vim.keymap.set("n", "n", "nzz", {
+  desc = "Go to next find and center screen and display line number",
+  noremap = true,
+  silent = true,
+})
+
+vim.keymap.set("n", "N", "Nzz", {
+  desc = "Go to next find and center screen and display line number",
+  noremap = true,
+  silent = true,
+})
---
+++ nvim/lua/plugins/theme.lua
@@ -0,0 +1,23 @@
+return {
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "tokyonight",
+    },
+  },
+  {
+    "Mofiqul/dracula.nvim",
+    name = "dracula",
+    priority = 1000,
+  },
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
+  },
+  {
+    "folke/tokyonight.nvim",
+    name = "tokyonight",
+    priority = 1000,
+  },
+}
