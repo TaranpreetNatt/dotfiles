7f09fd355d71bd08833281a283a298d987490158
TaranpreetNatt <taranpreetnatt12@hotmail.com>
2024-07-26 11:17:26 -0700

    fixed conform to work with lazyvim

--- nvim/lua/plugins/conform.lua
+++ nvim/lua/plugins/conform.lua
@@ -1,18 +1,11 @@
 return {
   "stevearc/conform.nvim",
-  event = { "BufReadPre", "BufNewFile" },
-  config = function()
-    local conform = require("conform")
-
-    conform.setup({
-      formatters_by_ft = {
-        python = { "black" },
-      },
-      format_on_save = {
-        lsp_fallback = true,
-        async = false,
-        timeout_ms = 500,
-      },
-    })
-  end,
+  opts = {
+    formatters_by_ft = {
+      python = { "black" },
+      lua = { "stylua" },
+      fish = { "fish_indent" },
+      sh = { "shfmt" },
+    },
+  },
 }
